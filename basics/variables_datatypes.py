bb3b9553607f4f784e73c209ccf9b9efb4415948
Aritra Chatterjee <ach142812@gmail.com>
2025-06-21 06:00:12 +0000

    types added

--- basics/variables_datatypes.py
+++ basics/variables_datatypes.py
@@ -1,23 +1,4 @@
-# Variables and Datatypes
-
-# Integer
-a = 1 
-
-# Floating Point Number
-b = 2.65
-
-# String
-name = "tony"
-
-# Boolean (True/False)
-isCorrect = True
-isWrong = False
-
-# None - None is a special constant used to represent the absence of a value. It is not equivalent to 0.
-c = None
-
-print(a+b)
-print(name)
+# Variables, Datatypes, and Type
 
 # Rules for variables
 a = 23
@@ -27,3 +8,45 @@ _tony = 34
 # @tony = 87 # Error - invalid
 # 12age = 12 # Variables cannot start with digits
 # full name = "Tony Stark" # no whitespaces allowed inside varibale name
+
+# Datatypes and Type
+
+# Integer
+a = 10
+print("a =", a, "| Type:", type(a))
+
+# Float
+b = 3.14
+print("b =", b, "| Type:", type(b))
+
+# String
+c = "Hello, Python"
+print("c =", c, "| Type:", type(c))
+
+# Boolean (True/False)
+d = True
+print("d =", d, "| Type:", type(d))
+
+# List
+e = [1, 2, 3]
+print("e =", e, "| Type:", type(e))
+
+# Tuple
+f = (1, 2, 3)
+print("f =", f, "| Type:", type(f))
+
+# Dictionary
+g = {"name": "Alice", "age": 25}
+print("g =", g, "| Type:", type(g))
+
+# Set
+h = {1, 2, 3}
+print("h =", h, "| Type:", type(h))
+
+# NoneType - None is a special constant used to represent the absence of a value. It is not equivalent to 0.
+i = None
+print("i =", i, "| Type:", type(i))
+
+# Complex
+j = 2 + 3j
+print("j =", j, "| Type:", type(j))
