627a7b27358d843e1a8194e5ca1f9da232d280cd
Aritra Chatterjee <ach142812@gmail.com>
2025-06-21 05:35:05 +0000

    Variables & Datatypes

---
+++ basics/variables_datatypes.py
@@ -0,0 +1,29 @@
+# Variables and Datatypes
+
+# Integer
+a = 1 
+
+# Floating Point Number
+b = 2.65
+
+# String
+name = "tony"
+
+# Boolean (True/False)
+isCorrect = True
+isWrong = False
+
+# None - None is a special constant used to represent the absence of a value. It is not equivalent to 0.
+c = None
+
+print(a+b)
+print(name)
+
+# Rules for variables
+a = 23
+aaa = 54
+tony = 45
+_tony = 34
+# @tony = 87 # Error - invalid
+# 12age = 12 # Variables cannot start with digits
+# full name = "Tony Stark" # no whitespaces allowed inside varibale name
