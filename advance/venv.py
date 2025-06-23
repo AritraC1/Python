927d81a49a4e49f6b7be04eea14cfbecbeccfee9
Aritra Chatterjee <ach142812@gmail.com>
2025-06-23 07:08:40 +0000

    virtual environment

---
+++ advance/venv.py
@@ -0,0 +1,12 @@
+# Python Virtual Environment
+
+# A virtual environment is an isolated Python environment.
+# It allows you to install and manage dependencies **specific to a project**,
+# without affecting the global Python installation.
+
+# This is useful when:
+# You have multiple projects with different dependencies
+# You want to avoid permission issues with global installs
+# You're working in a team and need to reproduce environments reliably
+
+# to install: pip install virtualenv
