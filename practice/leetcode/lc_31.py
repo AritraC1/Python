0b296a670066a2efc5430d0836e9bbd78cf6d710
Aritra Chatterjee <ach142812@gmail.com>
2025-07-20 13:48:02 +0530

    LC 31 next permutation

--- practice/leetcode/lc_31.py
+++ practice/leetcode/lc_31.py
@@ -34,8 +34,21 @@ Constraints:
 
 from typing import List
 
-
 def nextPermutation(self, nums: List[int]) -> None:
-    pass
+    i = len(nums) - 1
+    
+    while i > 0 and nums[i-1] >= nums[i]:
+        i -= 1
+        
+    if i == 0:
+        nums.reverse()
+        return
+        
+    j = len(nums) - 1
+        
+    while j >= i and nums[j] <= nums[i-1]:
+        j -= 1
+        
+    nums[i-1], nums[j] = nums[j], nums[i-1]
 
-n = int(input("Enter the size of the array: "))
+    nums[i:] = reversed(nums[i:])
